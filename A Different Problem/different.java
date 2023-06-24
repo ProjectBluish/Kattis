f624190c40ababd747ba0a62f52933e95cba578e
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-06-24 13:32:16 -0500

    Create different.java

---
+++ A Different Problem/different.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class different {
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    while (sc.hasNextLong()) {
+      long n1 = sc.nextLong(), n2 = sc.nextLong();
+
+      System.out.println(Math.abs(n1 - n2));
+    }
+
+    sc.close();
+  }
+}
