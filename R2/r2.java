205d8f4bf1f468ee93b4db5c1f6a6726340e630a
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-06-26 18:56:05 -0500

    Create r2.java

---
+++ R2/r2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class r2{
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int r1 = sc.nextInt(), s = sc.nextInt();
+        
+        System.out.println(2 * s - r1);
+        
+        sc.close();
+    }
+    
+}
