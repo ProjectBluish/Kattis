3e5b197267c6711f51dc96aa4f2d8a867fcae877
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-06-26 18:37:50 -0500

    Create cd.cpp

---
+++ CD/cd.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+//Sorted Inputs
+int BinarySearch(int arr[], int target, int size) {
+    int lo = 0, hi = size - 1;
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] < target)
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int N, M;
+    while (true) {
+        cin >> N >> M;
+        int count = 0;
+        if (N == 0 && M == 0)
+            break;
+        int cds[N];
+        for (int i = 0; i < N; i++) {
+            cin >> cds[i];
+        }
+        for (int j = 0; j < M; j++) {
+            int query;
+            cin >> query;
+            if (BinarySearch(cds, query, N) >= 0) {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
