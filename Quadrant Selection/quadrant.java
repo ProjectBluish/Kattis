d459de80100af63bf95e80dfba9f95da383409a9
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-06-26 19:01:39 -0500

    Create quadrant.java

---
+++ Quadrant Selection/quadrant.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class quadrant{
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt(), y = sc.nextInt();
+        
+        System.out.println(x > 0 ? (y > 0 ? 1 : 4) : (y > 0 ? 2 : 3));
+        
+        sc.close();
+    }
+    
+}
