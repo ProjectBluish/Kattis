0872c2c68ef516b99d8ac50e7ed599dc75d3752e
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-06-27 13:37:33 -0500

    Create unionfind.cpp

---
+++ Union-Find/unionfind.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> id;
+vector<int> sz;
+
+int Root(int a) {
+    if (id[a] != a) {
+        id[a] = Root(id[a]);
+    }
+    return id[a];
+}
+
+void Union(int a, int b) {
+    a = Root(a);
+    b = Root(b);
+
+    if (a == b) {
+        return;
+    }
+
+    if (sz[a] < sz[b]) {
+        id[a] = b;
+        sz[b] += sz[a];
+    } else {
+        id[b] = a;
+        sz[a] += sz[b];
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, Q;
+    cin >> N >> Q;
+
+    id.resize(N + 1);
+    sz.resize(N + 1, 1);
+    for (int i = 1; i <= N; i++) {
+        id[i] = i;
+    }
+
+    for (int i = 0; i < Q; i++) {
+        char query;
+        cin >> query;
+
+        int p, q;
+        cin >> p >> q;
+
+        if (query == '?') {
+            if (Root(p) == Root(q)) {
+                cout << "yes\n";
+            } else {
+                cout << "no\n";
+            }
+        } else {
+            Union(p, q);
+        }
+    }
+
+    return 0;
+}
