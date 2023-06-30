37fe329d173dded03bf24c2e398cc042f1c5fc7b
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-06-30 11:42:45 -0500

    Create echoechoecho.java

---
+++ Echo Echo Echo/echoechoecho.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class echoechoecho {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        String sample = sc.nextLine();
+        
+        System.out.println(sample + " " + sample + " " + sample);
+        
+        sc.close();
+        
+    }
+}
