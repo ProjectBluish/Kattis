8718cd343f41e680124040883095ff5f84e1b0da
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-07-06 14:18:04 -0500

    Create listgame.java

---
+++ A List Game/listgame.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class listgame {
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      
+      long n = sc.nextLong();
+      int count = 0;
+      
+      while (n % 2 == 0) {
+         count++;
+         n /= 2;
+      }
+      
+      for (int i = 3; i <= Math.sqrt(n); i += 2) {
+         while (n % i == 0) {
+            count++;
+            n /= i;
+         }
+      }
+      
+      if (n > 2) {
+         count++;
+      }
+      
+      System.out.println(count);
+      sc.close();
+   }
+}
