c579a2c719c1d37285fd052233718504537ddf55
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-07-06 16:07:36 -0500

    Create aaah.java

---
+++ Aaah!/aaah.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class aaah {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        String jm = sc.nextLine(), doc = sc.nextLine();
+        
+        sc.close();
+        System.out.println(jm.length() >= doc.length() ? "go" : "no");
+    }
+}
