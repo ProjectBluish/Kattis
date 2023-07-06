472ecaeab8a148614c9c72fd53531dc4e7f3be8a
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-07-06 13:41:46 -0500

    Create babelfish.java

---
+++ Babelfish/babelfish.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class babelfish {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String, String> babelFish = new HashMap<>();
+
+        while (sc.hasNext()) {
+            String in = sc.nextLine();
+
+            if (in.isEmpty()) {
+                continue;
+            }
+
+            if (in.contains(" ")) {
+                String[] temp = in.split(" ");
+                String key = temp[1], value = temp[0];
+                babelFish.put(key, value);
+            } else {
+                String translation = babelFish.getOrDefault(in, "eh");
+                System.out.println(translation);
+            }
+        }
+        sc.close();
+    }
+}
