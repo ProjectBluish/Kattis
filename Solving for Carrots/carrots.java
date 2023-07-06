92fcd65dbae706f986bdaf7b953fc256ec03e347
Tony <137019226+ProjectBluish@users.noreply.github.com>
2023-07-06 16:02:34 -0500

    Create carrots.java

---
+++ Solving for Carrots/carrots.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class carrots {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt(), P = sc.nextInt();
+        sc.nextLine();
+        while (sc.hasNext()) {
+            sc.nextLine();
+        }
+        System.out.print(P);
+        sc.close();
+    }
+}
